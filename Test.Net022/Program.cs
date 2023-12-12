1fe16710b2d785c014f413100f70715ad527eab1
div-S <sadanandan.div@gmail.com>
2023-12-11 20:45:02 -0500

    Added first test

---
+++ Test.Net022/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Test.Net022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Test1<int> list = new Test1<int>();
+            list.AddItem(5);
+            list.AddItem(2);
+            list.AddItem(8);
+
+            int itemAtIndex = list.GetValue(1);
+            Console.WriteLine(itemAtIndex);
+
+        }
+    }
+}
---
+++ Test.Net022/Test1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Net022
+{
+    public class Test1<T> where T : struct 
+    {
+        private List<T> list;
+
+        public Test1()
+        {
+            list = new List<T>();
+        }
+
+        public void AddItem(T item) 
+        {
+            list.Add(item);
+        }
+
+        public T GetValue(int i)
+        {
+            return list[i];
+        }
+
+        public List<T> GetSorted()
+        {
+            List<T> sortedList = list.OrderByDescending(x => x).ToList();
+            return sortedList;
+        }
+    }
+}
